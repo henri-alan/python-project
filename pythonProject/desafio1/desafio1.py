3f979a85ca98e09ce06d4f5df4ae409e7893b3bd
henri-alan <alanhenridev@gmail.com>
2024-10-23 18:24:36 +0000

    Update desafio1.py
    apenas ajustando a identacao

--- pythonProject/desafio1/desafio1.py
+++ pythonProject/desafio1/desafio1.py
@@ -14,4 +14,4 @@ peso = 143
 current_year = 2024
 imc = peso / (altura * altura)
 
-print('O nome da pessoa e {n}, a idade e {i} anos, com {a}cm de altura, e {p} kg, o ano de nascimento e {ci} e o imc e {im:.2f}'.format(n=nome, i= idade, a=altura, p= peso, ci=current_year -idade, im=imc ))
+print('O nome da pessoa e {n}, a idade e {i} anos, com {a}cm de altura, e {p} kg, o ano de nascimento e {ci} e o imc e {im:.2f}'.format(n=nome, i= idade, a=altura, p= peso, ci=current_year-idade, im=imc ))
